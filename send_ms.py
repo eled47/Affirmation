cfe737558e42fe754e89bac3af2c7d1d576fd90f
Edward Ellis <33016329+eled47@users.noreply.github.com>
2020-11-03 12:58:48 -0500

    Update send_ms.py

--- send_ms.py
+++ send_ms.py
@@ -4,15 +4,15 @@ from twilio.rest import Client
 
 # Your Account Sid and Auth Token from twilio.com/console
 # DANGER! This is insecure. See http://twil.io/secure
-account_sid = 'AC6f2e1e50d879fdf510e86edcc16c9f3c'
-auth_token = 'f5f0629ea6c74c891652a072e5d79f83'
+account_sid = 'sid'
+auth_token = 'token'
 client = Client(account_sid, auth_token)
 
 message = client.messages \
                 .create(
                      body="Join Earth's mightiest heroes. Like Kevin Bacon.",
-                     from_='+16087955604',
-                     to='+14235809433'
+                     from_='account_number',
+                     to='phone_number'
                  )
 
 print(message.sid)
